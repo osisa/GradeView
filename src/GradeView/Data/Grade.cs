d5725769e8dad4af534cca0d23e877b2489a796e
Joël Schöni <pisshead2@hotmail.com>
2019-07-30 15:40:42 +0200

    Initial Upload

---
+++ src/GradeView/Data/Grade.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace GradeView.Data
+{
+    public class Grade
+    {
+        public DateTime Date { get; set; }
+
+        public int Value { get; set; }
+
+        public string Subject { get; set; }
+    }
+}
---
+++ src/GradeView/Data/Subject.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace GradeView.Data
+{
+    public class Subject
+    {
+        public string Text { get; set; }
+    }
+}

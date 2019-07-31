a6229c8fd74c112476695cfc4ed5b7283aa21e21
soerenOsisa <53479110+soerenOsisa@users.noreply.github.com>
2019-07-31 11:10:54 +0200

    azure added pipeline

---
+++ SolutionInfo.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="o.s.i.s.a. GmbH" file="SolutionInfo.cs">
+//    (c) 2014. See licence text in binary folder.
+// </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+using System.Security;
+
+[assembly: SecurityRules(SecurityRuleSet.Level1)]
+
+internal static class SolutionInfo
+{
+    #region Constants
+
+    /// <summary>
+    ///     Solution wide company name for the assemblyInfo files.
+    /// </summary>
+    public const string Company = "open source";
+
+    /// <summary>
+    ///     Solution wide copyright description for the assemblyInfo files.
+    /// </summary>
+    public const string Copyright = "no copyrights";
+
+    /// <summary>
+    ///     Solution wide product description for the assemblyInfo files.
+    /// </summary>
+    public const string Product = "Grade View";
+
+    /// <summary>
+    ///     Solution wide trademark description for the assemblyInfo files.
+    /// </summary>
+    public const string Trademark = "GradeView";
+
+    #endregion
+}

9aedf90b9db9d408a3aaf9dd431e08b7520ad7f6
maske-one <s@maske.one>
2019-08-27 20:47:50 +0200

    commit

---
+++ src/GradeView.Tests/Class1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using FluentAssertions;
+
+namespace GradeView.Tests
+{
+    [TestClass]
+    public class GradeViewBasicTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            // arrange
+            var argument1 = 2;
+
+            //act
+            var result = argument1 * 3;
+
+            //assert
+            Assert.AreEqual(6, result);
+        }
+    }
+}
